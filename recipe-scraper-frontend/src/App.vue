9f502fd2b5ffdf25c1b5e8d217ce00fff7fd865b
Lilian <81225510+mintchococookies@users.noreply.github.com>
2025-06-07 14:43:05 +0800

    Randomize the loading text

--- recipe-scraper-frontend/src/App.vue
+++ recipe-scraper-frontend/src/App.vue
@@ -559,14 +559,25 @@ export default {
     };
   },
   methods: {
+    shuffleArray(array) {
+      const shuffled = [...array];
+      for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+      }
+      return shuffled;
+    },
+
     startLoadingAnimation() {
-      this.currentLoadingText = this.loadingTexts[0];
+      // Shuffle the loading texts array
+      const shuffledTexts = this.shuffleArray(this.loadingTexts);
+      this.currentLoadingText = shuffledTexts[0];
       this.loadingTextIndex = 0;
       
-      // Change text every 1 seconds
+      // Change text every 700ms using the shuffled array
       this.loadingInterval = setInterval(() => {
-        this.loadingTextIndex = (this.loadingTextIndex + 1) % this.loadingTexts.length;
-        this.currentLoadingText = this.loadingTexts[this.loadingTextIndex];
+        this.loadingTextIndex = (this.loadingTextIndex + 1) % shuffledTexts.length;
+        this.currentLoadingText = shuffledTexts[this.loadingTextIndex];
       }, 700);
     },
     
